c9b24e68f953dc77d6e1d937673da3ff4c1a8806
rvkhaustov <romaz_72@mail.ru>
2017-04-05 15:26:49 +0700

    Added 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,19 @@
 package ru.job4j;
+/** 
+ * Class Calculate 1 занятие
+ * @author rkhaustov
+ * @since 05.04.2017
+ * @version 1
+*/ 
+
 
 public class Calculate {
- public static void main(String[] args) {
- System.out.println("Hello World");
- }
+/**
+ * Метод main, вывод строки в консоль
+ * @param args - arg
+ */
+    
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
 }
