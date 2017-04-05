cd6f17ba5fddf83ff1fa88d247d5ec673ab2537a
rvkhaustov <romaz_72@mail.ru>
2017-04-05 21:21:02 +0700

    Task 4123

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,18 +1,17 @@
 package ru.job4j;
-/** 
- * Class Calculate 1 занятие
- * @author rkhaustov
- * @since 05.04.2017
- * @version 1
-*/ 
 
-
-public class Calculate {
 /**
- * Метод main, вывод строки в консоль
- * @param args - arg
+ * Calculate.
+ *
+ * @author Roman Khaustov (romavsb@gmail.com)
+ * @version $Id$
+ * @since 0.1
  */
-    
+public class Calculate {
+    /**
+     * main.
+     * @param args - arg.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
