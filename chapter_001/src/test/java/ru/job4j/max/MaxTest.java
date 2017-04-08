9b6088fa0356bc73f4bca289a2890bac996a4a11
rvkhaustov <romaz_72@mail.ru>
2017-04-08 10:18:15 +0700

    Add 189

---
+++ chapter_001/src/test/java/ru/job4j/max/MaxTest.java
@@ -0,0 +1,24 @@
+package ru.job4j.max;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ *
+ * @author Roman Khaustov
+ * @version $Id$
+ * @since 0.1
+ */
+public class MaxTest {
+  /**
+   * Test Max.
+   */
+   @Test
+    public void whenMaxThreeTwoThenThree() {
+    Max maxValue = new Max();
+    int result = maxValue.max(3, 2);
+    int expected =  3;
+    assertThat(result, is(expected));
+    }
+}
