009918d145e1b93c9c28983aec5c5eac977cf4b2
rvkhaustov <romaz_72@mail.ru>
2017-05-26 09:33:43 +0700

    Тестовое задание #10038

--- chapter_005/src/main/java/ru/rkhaustov/bank/Action.java
+++ chapter_005/src/main/java/ru/rkhaustov/bank/Action.java
@@ -28,8 +28,6 @@ public class Action {
     @Override
     public String toString() {
         return String.format("%s", userAccount);
-//        return String.format("userAccount=%s", userAccount);
-
     }
 
     /**
@@ -118,59 +116,4 @@ public class Action {
         }
         return correct;
     }
-
-//    public static void main(String[] args) {
-//        Action action = new Action();
-//        User user = new User("Igor", "1234567");
-//        action.addUser(new User("Igor", "1234567"));
-//        action.addAccountToUser(new User("Igor", "1234567")
-//                ,new Account("1111111111", 100));
-//        action.addAccountToUser(new User("Igor", "1234567")
-//                ,new Account("222222", 0));
-//        action.addAccountToUser(new User("Igor", "1234567")
-//                ,new Account("11111111112", 0));
-//
-//        User userDima = new User("Dima", "7654321");
-//
-//        action.addUser(new User("Dima", "7654321"));
-//        action.addAccountToUser(new User("Dima", "7654321")
-//                ,new Account("D1111111111", 0));
-//        action.addAccountToUser(new User("Dima", "7654321")
-//                ,new Account("D222222", 0));
-//        action.addAccountToUser(new User("Dima", "7654321")
-//                ,new Account("D11111111112", 0));
-//
-//        //        action.deleteUser(new User("Igor", "1234567"));
-////        for (Map.Entry<User,List<Account>> entry : action.userAccount.entrySet()) {
-////            System.out.println(entry.getKey() + " " + entry.getValue() );
-////        }
-////        System.out.println("deleteAccountFromUser");
-////        action.deleteAccountFromUser(new User("Dima", "7654321"), new Account("D1111111111", 10));
-////
-////        System.out.println(action.getUserAccounts(new User("Igor", "1234567")));
-//
-////        System.out.println(action.userAccount.toString());
-//
-//
-//
-////        action.deleteUser(new User("Igor", "1234567"));
-////        System.out.println(action.userAccount.toString());
-//        System.out.println("befor");
-//        for (Map.Entry<User,List<Account>> entry : action.userAccount.entrySet()) {
-//            System.out.println(entry.getKey() + " " + entry.getValue() );
-//        }
-//
-//        System.out.println(action.transferMoney(user
-//                , new Account("1111111111")
-//                ,userDima
-//                , new Account("D222222"), 100));
-//        System.out.println("after");
-//        for (Map.Entry<User,List<Account>> entry : action.userAccount.entrySet()) {
-//            System.out.println(entry.getKey() + " " + entry.getValue() );
-//        }
-////        for( Map.Entry<String, String> entry : map.entrySet() ){
-////            System.out.println( entry.getKey() + " " + entry.getValue() );
-////        }
-////        Action.this.addUser(new User("Igor", "1234567"));
-//    }
 }
