7e177044162e98de5eef234f054a517629c808be
rvkhaustov <romaz_72@mail.ru>
2017-05-30 22:40:12 +0700

    Создать контейнер на базе связанного списка #159

--- chapter_005/src/main/java/ru/rkhaustov/list/LinkedArray.java
+++ chapter_005/src/main/java/ru/rkhaustov/list/LinkedArray.java
@@ -1,19 +1,40 @@
 package ru.rkhaustov.list;
 
 import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.NoSuchElementException;
+
 
 /**
  * Created by rvkha_000 on 30.05.2017.
+ * @param <E> type object.
  */
-public class LinkedArray implements SimpleContainer<E> {
+public class LinkedArray<E> implements SimpleContainer<E> {
+    /**
+     * size array.
+     */
+    private int size = 0;
+    /**
+     * Ponter first value.
+     */
+    private Node<E> first;
+    /**
+     * Ponter last value.
+     */
+    private Node<E> last;
     /**
      * @param e value objects.
      */
     @Override
     public void add(E e) {
-     //   List list = new LinkedList()
+        final Node<E> l = last;
+        final Node<E> newNode = new Node<>(l, e, null);
+        last = newNode;
+        if (l == null) {
+            first = newNode;
+        } else {
+            l.next = newNode;
+        }
+        size++;
     }
 
     /**
@@ -22,8 +43,21 @@ public class LinkedArray implements SimpleContainer<E> {
      */
     @Override
     public E get(int index) {
-        return null;
+        if (index < (size >> 1)) {
+            Node<E> node = first;
+            for (int atFirst = 0; atFirst < index; atFirst++) {
+                node = node.next;
+            }
+            return node.object;
+        } else {
+            Node<E> node = last;
+            for (int fromEnd = size - 1; fromEnd > index; fromEnd--) {
+                node = node.prev;
+            }
+            return node.object;
+        }
     }
+
 
     /**
      * Returns an iterator over elements of type {@code T}.
@@ -32,6 +66,78 @@ public class LinkedArray implements SimpleContainer<E> {
      */
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new LinkedArrayIterator();
+    }
+
+    /**
+     * Node class storege pointer.
+     * @param <E> type object.
+     */
+    public static class Node<E> {
+        /**
+         * object.
+         */
+        private E  object;
+        /**
+         * next pointer.
+         */
+        private Node<E> next;
+        /**
+         * prev pointer.
+         */
+        private Node<E> prev;
+
+        /**
+         * @param prev prev pointer.
+         * @param element object.
+         * @param next next pointer.
+         */
+        Node(Node<E> prev, E element, Node<E> next) {
+            this.object = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+    /**
+     * implements Iterator methods.
+     */
+    public class LinkedArrayIterator implements Iterator<E> {
+        /**
+         * indexIterator.
+         */
+        private int indexIterator = 0;
+        /**
+         * nodeIterator.
+         */
+        private Node<E> nodeIterator = first;
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         */
+        @Override
+        public boolean hasNext() {
+            return indexIterator < size ? true : false;
+        }
+
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         */
+        @Override
+        public E next() {
+            if (!hasNext()) {
+              throw new NoSuchElementException();
+            }
+            if (indexIterator++ != 0) {
+                nodeIterator = nodeIterator.next;
+            }
+            return nodeIterator.object;
+        }
     }
 }
---
+++ chapter_005/src/test/java/ru/rkhaustov/list/LinkedArrayTest.java
@@ -0,0 +1,71 @@
+package ru.rkhaustov.list;
+
+import org.junit.Test;
+import java.util.Iterator;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Created by rvkha_000 on 30.05.2017.
+ */
+public class LinkedArrayTest {
+    /**
+     * test method add and get <String>.
+     */
+    @Test
+    public void  whenAddStringThenGetString() {
+        SimpleContainer<String> list = new LinkedArray<>();
+
+        list.add("test");
+        String result = list.get(0);
+
+        assertThat(result, is("test"));
+    }
+    /**
+     * test method next() <Integer>.
+     */
+    @Test
+    public void whenNextThenPointerForward() {
+        SimpleContainer<Integer> list = new LinkedArray<>();
+
+        list.add(4);
+        list.add(3);
+        list.add(2);
+        list.add(1);
+
+        Iterator<Integer> iterator = list.iterator();
+
+        int result = iterator.next();
+        assertThat(result, is(4));
+
+        result = iterator.next();
+        assertThat(result, is(3));
+
+        result = iterator.next();
+        assertThat(result, is(2));
+
+        result = iterator.next();
+        assertThat(result, is(1));
+    }
+    /**
+     * Test hasNext() <Float>.
+     */
+    @Test
+    public void whenHasNextCheckNextThenReturnFalse() {
+        SimpleContainer<Float> list = new LinkedArray<>();
+
+        list.add(4f);
+        list.add(3f);
+
+
+        Iterator<Float> iterator = list.iterator();
+
+        iterator.next();
+        iterator.next();
+
+        boolean result = iterator.hasNext();
+
+        assertThat(result, is(false));
+    }
+}
