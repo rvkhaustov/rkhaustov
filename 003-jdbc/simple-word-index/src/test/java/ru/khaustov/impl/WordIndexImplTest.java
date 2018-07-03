caa2f68dc3a117ad6cf81b868d79592e23565821
rvkhaustov <romaz_72@mail.ru>
2018-07-03 09:30:27 +0700

    Задача ПИТЕР-СЕРВИС [ #50316] Compare Indexes4Word with String.index.Of

--- 003-jdbc/simple-word-index/src/test/java/ru/khaustov/impl/WordIndexImplTest.java
+++ 003-jdbc/simple-word-index/src/test/java/ru/khaustov/impl/WordIndexImplTest.java
@@ -5,9 +5,13 @@ import org.junit.Test;
 import ru.khaustov.WordIndex;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
+import static org.hamcrest.core.Is.is;
 
 /**
  * Created by rvkha_000 on 24.05.2018.
@@ -46,5 +50,16 @@ public class WordIndexImplTest {
         assertTrue(result.size() == 2);
         assertTrue(result.contains(167));
         assertTrue(result.contains(222));
+    }
+
+    /**
+     * Compare Indexes4Word with String.index.Of .
+     */
+    @Test
+    public void compareIndexes4WordWithIndexOf() {
+        List<Integer> integers = new ArrayList<>(wordIndex.getIndexes4Word("a"));
+        String str = "The parser will make every attempt parser to create a clean parse from the HTML you provide, regardless of whether the HTML is well-formed or not. It handles:";
+        Integer expected = str.indexOf('a');
+        assertThat(expected, is(integers.get(0)));
     }
 }
